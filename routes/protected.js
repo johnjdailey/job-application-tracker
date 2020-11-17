0426f7606339ba099a2a7272f5f5492e71a54c6c
Chris <chrisjones0517@gmail.com>
2020-11-16 19:21:15 -0600

    removed json web token auth

--- routes/protected.js
+++ routes/protected.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 const { addCompany, getCompanies, updateCompany, deleteCompanies } = require('../controllers/company');
 const { getTemplates, addTemplate, deleteTemplate, updateTemplate } = require('../controllers/template');
-
-router.use(verifyToken);
 
 router
     .route('/')
@@ -30,25 +27,4 @@ router
     .post(updateTemplate);
 
 module.exports = router;
-
-function verifyToken(req, res, next) {
-    const token = req.headers['token'];
-
-    if (token !== null) {
-        req.token = token;
-    } else {
-        console.log('token', token);
-        res.sendStatus(401);
-    }
-
-    jwt.verify(req.token, process.env.SECRET_KEY, (err, authData) => {
-        if (err) {
-            return res.sendStatus(401)
-        } else {
-            next();
-        }
-    })
-}
-
-
 
